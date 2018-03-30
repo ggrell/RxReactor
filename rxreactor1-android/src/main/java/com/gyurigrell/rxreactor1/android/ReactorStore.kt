8ae50f4e86b681fa4918623b6a08fb7351a4844c
Gyuri Grell <ggrell@pobox.com>
2018-03-30 16:49:40 -0400

    Added ViewModel-style providers for Reactor instances that help with config changes * Classes are implemented (too bad I need two versions of the same code for RxJava 1 vs 2, is there a better way?) * Still need to fix android library output file naming, and add sources/javadoc artifacts

---
+++ rxreactor1-android/src/main/java/com/gyurigrell/rxreactor1/android/ReactorStore.kt
@@ -0,0 +1,65 @@
+package com.gyurigrell.rxreactor1.android
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import com.gyurigrell.rxreactor1.Reactor
+import com.gyurigrell.rxreactor1.android.ReactorStoreFragment.Companion.reactorStoreFragmentFor
+
+/**
+ * Class to store [Reactor]s.
+ *
+ * An instance of `ReactorStore` must be retained through configuration changes:
+ * if an owner of this `ReactorStore` is destroyed and recreated due to configuration
+ * changes, new instance of an owner should still have the same old instance of
+ * `ReactorStore`.
+ *
+ * If an owner of this `ReactorStore` is destroyed and is not going to be recreated,
+ * then it should call [clear] on this `ReactorStore`, so `Reactors` would
+ * be notified that they are no longer used.
+ *
+ * [ReactorStore.of] provides a `ReactorStore` for activities and fragments.
+ */
+class ReactorStore {
+    private var store = HashMap<String, Reactor<*, *, *>>()
+
+    fun <Action, Mutation, State> get(key: String): Reactor<Action, Mutation, State>? {
+        @Suppress("UNCHECKED_CAST")
+        return store[key] as? Reactor<Action, Mutation, State>
+    }
+
+    fun <Action, Mutation, State> put(key: String, reactor: Reactor<Action, Mutation, State>) {
+        store[key] = reactor
+    }
+
+    /**
+     * Clears internal storage and notifies ViewModels that they are no longer used.
+     */
+    fun clear() {
+//        for (reactor in store.values) {
+//            reactor.onCleared()
+//        }
+        store.clear()
+    }
+
+    companion object {
+        /**
+         * Returns the [ReactorStore] of the given fragment.
+         *
+         * @param fragment a fragment whose `ReactorStore` is requested
+         * @return a `ReactorStore`
+         */
+        fun of(fragment: Fragment): ReactorStore {
+            return reactorStoreFragmentFor(fragment).reactorStore
+        }
+
+        /**
+         * Returns the [ReactorStore] of the given activity.
+         *
+         * @param activity an activity whose `ReactorStore` is requested
+         * @return a `ReactorStore`
+         */
+        fun of(activity: FragmentActivity): ReactorStore {
+            return reactorStoreFragmentFor(activity).reactorStore
+        }
+    }
+}
---
+++ rxreactor2-android/src/main/java/com/gyurigrell/rxreactor2/android/ReactorStoreFragment.kt
@@ -0,0 +1,157 @@
+package com.gyurigrell.rxreactor2.android
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import android.support.annotation.RestrictTo
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import android.support.v4.app.FragmentManager
+
+import java.util.*
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+class ReactorStoreFragment @SuppressLint("ValidFragment") constructor(
+    val reactorStore: ReactorStore = ReactorStore()) : Fragment() {
+
+    init {
+        retainInstance = true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        reactorStoreFragmentManager.reactorStoreFragmentCreated(this)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        reactorStore.clear()
+    }
+
+    companion object {
+        private val reactorStoreFragmentManager = ReactorStoreFragmentManager()
+
+        fun reactorStoreFragmentFor(fragment: Fragment) : ReactorStoreFragment {
+            return reactorStoreFragmentManager.reactorStoreFragmentFor(fragment)
+        }
+
+        fun reactorStoreFragmentFor(activity: FragmentActivity) : ReactorStoreFragment {
+            return reactorStoreFragmentManager.reactorStoreFragmentFor(activity)
+        }
+    }
+
+    internal class ReactorStoreFragmentManager {
+        private val notCommittedActivityHolders = HashMap<Activity, ReactorStoreFragment>()
+        private val notCommittedFragmentHolders = HashMap<Fragment, ReactorStoreFragment>()
+
+        private val activityCallbacks = object : Application.ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity?) {}
+
+            override fun onActivityResumed(activity: Activity?) {}
+
+            override fun onActivityStarted(activity: Activity?) {}
+
+            override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {}
+
+            override fun onActivityStopped(activity: Activity?) {}
+
+            override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {}
+
+            override fun onActivityDestroyed(activity: Activity?) {
+                val fragment = notCommittedActivityHolders.remove(activity!!)
+                if (fragment != null) {
+//                    Log.e(LOG_TAG, "Failed to save a ViewModel for " + activity!!)
+                }
+            }
+        }
+
+        private var activityCallbacksIsAdded = false
+
+        private val parentDestroyedCallback = object : FragmentManager.FragmentLifecycleCallbacks() {
+            override fun onFragmentDestroyed(fm: FragmentManager?, parentFragment: Fragment?) {
+                super.onFragmentDestroyed(fm, parentFragment)
+                val fragment = notCommittedFragmentHolders.remove(parentFragment)
+                if (fragment != null) {
+//                    Log.e(LOG_TAG, "Failed to save a ViewModel for " + parentFragment!!)
+                }
+            }
+        }
+
+        fun reactorStoreFragmentCreated(holderFragment: Fragment) {
+            val parentFragment = holderFragment.parentFragment
+            if (parentFragment != null) {
+                notCommittedFragmentHolders.remove(parentFragment)
+                parentFragment.fragmentManager?.unregisterFragmentLifecycleCallbacks(parentDestroyedCallback)
+            } else {
+                notCommittedActivityHolders.remove(holderFragment.activity!!)
+            }
+        }
+
+        fun reactorStoreFragmentFor(activity: FragmentActivity): ReactorStoreFragment {
+            val fm = activity.supportFragmentManager
+            var reactorStoreFragment = findReactorStoreFragment(fm)
+            if (reactorStoreFragment != null) {
+                return reactorStoreFragment
+            }
+            reactorStoreFragment = notCommittedActivityHolders[activity]
+            if (reactorStoreFragment != null) {
+                return reactorStoreFragment
+            }
+
+            if (!activityCallbacksIsAdded) {
+                activityCallbacksIsAdded = true
+                activity.application.registerActivityLifecycleCallbacks(activityCallbacks)
+            }
+            reactorStoreFragment = createReactorStoreFragment(fm)
+            notCommittedActivityHolders[activity] = reactorStoreFragment
+            return reactorStoreFragment
+        }
+
+        fun reactorStoreFragmentFor(parentFragment: Fragment): ReactorStoreFragment {
+            val fm = parentFragment.childFragmentManager
+            var reactorStoreFragment = findReactorStoreFragment(fm)
+            if (reactorStoreFragment != null) {
+                return reactorStoreFragment
+            }
+            reactorStoreFragment = notCommittedFragmentHolders[parentFragment]
+            if (reactorStoreFragment != null) {
+                return reactorStoreFragment
+            }
+
+            parentFragment.fragmentManager?.registerFragmentLifecycleCallbacks(parentDestroyedCallback, false)
+            reactorStoreFragment = createReactorStoreFragment(fm)
+            notCommittedFragmentHolders[parentFragment] = reactorStoreFragment
+            return reactorStoreFragment
+        }
+
+        private fun findReactorStoreFragment(manager: FragmentManager): ReactorStoreFragment? {
+            if (manager.isDestroyed) {
+                throw IllegalStateException("Can't access ViewModels from onDestroy")
+            }
+
+            val fragmentByTag = manager.findFragmentByTag(STORE_FRAGMENT_TAG)
+            if (fragmentByTag != null && fragmentByTag !is ReactorStoreFragment) {
+                throw IllegalStateException("Unexpected fragment instance was returned by HOLDER_TAG")
+            }
+            return fragmentByTag as? ReactorStoreFragment
+        }
+
+        private fun createReactorStoreFragment(fragmentManager: FragmentManager): ReactorStoreFragment {
+            val newStoreFragment = ReactorStoreFragment()
+            fragmentManager
+                .beginTransaction()
+                .add(newStoreFragment, STORE_FRAGMENT_TAG)
+                .commitAllowingStateLoss()
+            return newStoreFragment
+        }
+
+        companion object {
+            private const val STORE_FRAGMENT_TAG = "com.gyurigrell.rxreactor.ReactorStoreFragment"
+        }
+    }
+}
