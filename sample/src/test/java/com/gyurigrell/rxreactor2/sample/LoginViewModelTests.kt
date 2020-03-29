573f7be177b7142981501f93c45be2597bc31c4c
Gyuri Grell <ggrell@pobox.com>
2020-03-29 17:35:31 -0400

    Updated copyright

--- sample/src/test/java/com/gyurigrell/rxreactor2/sample/LoginViewModelTests.kt
+++ sample/src/test/java/com/gyurigrell/rxreactor2/sample/LoginViewModelTests.kt
@@ -1,7 +1,11 @@
-package com.gyurigrell.rxreactor2.sample
+/*
+ * Copyright (c) 2020, Gyuri Grell and RxReactor contributors. All rights reserved
+ *  
+ * Licensed under BSD 3-Clause License. 
+ * https://opensource.org/licenses/BSD-3-Clause
+ */
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+package com.gyurigrell.rxreactor2.sample
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +13,4 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class LoginViewModelTests {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
 }
