e6ef9051f424b42ed865ee687fcef70a2fc7e050
Gyuri Grell <ggrell@pobox.com>
2020-04-02 14:44:41 -0400

    AndroidReactor & AndroidReactorWithEffects were missing `abstract` to be overridden * Cleaned up a couple lint warnings as well

--- rxreactor1/src/main/kotlin/com/gyurigrell/rxreactor1/SimpleReactor.kt
+++ rxreactor1/src/main/kotlin/com/gyurigrell/rxreactor1/SimpleReactor.kt
@@ -15,12 +15,10 @@ import rx.Observable
  * publicly available since actions are passed to the reactor via this type (using the {@see action} relay observer.
  * @param State the type of the state that the reactor holds and modifies.
  * @property initialState the initial state of the reactor, from which the {@see currentState} will be initialized.
- * @property debug default is false. When set to true, each action, mutation and state change is logged
  * via {@link logDebug}
  */
-abstract class SimpleReactor<Action, State>(initialState: State) :
-        Reactor<Action, Action, State>(initialState) {
-    override fun mutate(action: Action): Observable<Action> {
-        return Observable.just(action)
-    }
+abstract class SimpleReactor<Action, State>(
+    initialState: State
+) : Reactor<Action, Action, State>(initialState) {
+    override fun mutate(action: Action): Observable<Action> = Observable.just(action)
 }
